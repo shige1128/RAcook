0758b0f18dcf9ea42cdc6131a966d02e88e6491c
shige1128 <shigedeen@gmail.com>
2021-03-15 10:53:44 +0900

    画像保存機能(ビュー編集)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
   belongs_to :dish_portion
 
   belongs_to :user
+  has_one_attached :image
 
   has_many   :recipe_ingredients
   has_many   :dish_ingredients, through: :recipe_ingredients
@@ -10,7 +11,7 @@ class Recipe < ApplicationRecord
   has_many   :recipe_seasonings
   has_many   :seasonings, through: :recipe_seasonings
 
-  validates  :dish_name, :dish_portion_id, :step_1, presence: true
+  validates  :image, :dish_name, :dish_portion_id, :step_1, presence: true
   validates  :dish_portion_id, numericality: { other_than: 1 }
 
 end
