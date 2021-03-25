626a847f4ac9ea55c8dcb230b3dc53462c180daa
shige1128 <shigedeen@gmail.com>
2021-03-26 01:09:44 +0900

    コントローラーにコメント保存と表示のアクション設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,13 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: :create
 
   def create
-    Comment.create(comment_params)
+    comment = Comment.create(comment_params)
     redirect_to "/recipes/#{comment.recipe.id}"
   end
 
   private
 
   def comment_params
-    params.require(:cmment).permit(:text).merge(user_id: current_user.id, recipe_id: params[:recipe_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, recipe_id: params[:recipe_id])
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,8 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @comment = Comment.new
+    @comments = @recipe.comments.includes(:user)
   end
 
   def search
